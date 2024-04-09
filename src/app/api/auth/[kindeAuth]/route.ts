c4a85b82bd6a97fb5a70a1ead899220d0e6d765c
ankan321 <adalui260@gmail.com>
2024-04-09 22:29:22 +0530

    Initial Commit

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,9 +1,7 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextRequest } from "next/server";
 
-async function GET(request: NextRequest, { params }: any) {
+export async function GET(request: NextRequest, { params }: any) {
   const endpoint = params.kindeAuth;
   return handleAuth(request, endpoint);
 }
-
-export default GET;
