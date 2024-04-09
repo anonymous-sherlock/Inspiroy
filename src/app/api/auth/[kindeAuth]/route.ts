5f2b6d6753dfad60993be1c12a33285d6000acc5
ankan321 <adalui260@gmail.com>
2024-04-09 22:31:55 +0530

    Initial Commit

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,21 +1,7 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest } from "next/server";
 
-// Exporting the handler function for GET requests
-export default async function handler(
-  request: NextApiRequest,
-  response: NextApiResponse
-) {
-  const { kindeAuth } = request.query;
-
-  // Assuming `kindeAuth` is the endpoint parameter
-  if (request.method === "GET") {
-    // Handle GET request
-    const result = handleAuth(request, kindeAuth as string); // Cast kindeAuth to string
-    response.status(200).json(result);
-  } else {
-    // Handle other HTTP methods if necessary
-    response.setHeader("Allow", ["GET"]);
-    response.status(405).end(`Method ${request.method} Not Allowed`);
-  }
+export async function GET(request: NextRequest, { params }: any) {
+  const endpoint = params.kindeAuth;
+  return handleAuth(request, endpoint);
 }
