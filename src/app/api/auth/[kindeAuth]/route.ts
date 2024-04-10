cc24c6a20fcb54aca26dcf9f3f2c4af4422692d6
ankan321 <adalui260@gmail.com>
2024-04-10 09:02:12 +0530

    Initial Commit

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,9 +1,7 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-import { GetServerSideProps } from "next";
-import { NextRequest } from "next/server";
+// import { NextRequest } from "next/server";
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const endpoint = context.params?.kindeAuth as string; // Ensure endpoint is of type string
-  const response = handleAuth(context.req, endpoint);
-  return { props: { responseData: response } };
-};
+export async function GET(request: Request, { params }: any) {
+  const endpoint = params.kindeAuth;
+  return handleAuth(request, endpoint);
+}
