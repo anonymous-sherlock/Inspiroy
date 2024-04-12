65143d4a20734a9092d09420e9a5a5fa5772a5c8
ankan321 <adalui260@gmail.com>
2024-04-12 11:09:48 +0530

    Initial Commit

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,18 +1,7 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest, { params }: any) {
   const endpoint = params.kindeAuth;
-  const handler = await handleAuth(request, endpoint);
-
-  // Call the handler function and return its result
-  const result = await handler(request, { params });
-
-  // If the result is a NextResponse object, return it
-  if (result instanceof NextResponse) {
-    return result;
-  }
-
-  // Otherwise, create a new NextResponse object
-  return new NextResponse("OK");
+  return handleAuth(request, endpoint);
 }
